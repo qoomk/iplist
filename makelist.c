a025e1ba49ba210c095fb59bd18ba0dc758ff7c5
Kumiko Ohashi <kumiko.ohashi@altenergy.co.jp>
2019-02-22 12:43:59 +0900

    ip

--- makelist.c
+++ makelist.c
@@ -22,7 +22,7 @@ int main(int argc,char*argv[]){
 	char *listfile,*ip123;
 	int ip4s,ip4e;
 
-	printf("param %d\n",argc);
+	//printf("param %d\n",argc);
 	
 	if(argc != 9){
 		fprintf(stderr,"param %d\n",argc);
@@ -30,7 +30,7 @@ int main(int argc,char*argv[]){
 	}
 
 	while((opt=getopt(argc,argv,"l:i:s:e:"))!= -1){
-		printf("%c %s \n",opt,optarg);
+		//printf("%c %s \n",opt,optarg);
 		switch(opt){
 			case 'l':
 				listfile=optarg;
@@ -61,14 +61,14 @@ int main(int argc,char*argv[]){
 
 	fp = fopen(listfilewk,"wt");
 	if(fp==NULL){
-		printf("file open(create) %s\n",listfilewk);
+		//printf("file open(create) %s\n",listfilewk);
 		fprintf(stderr,"file open(create) %s\n",listfilewk);
 		return -1;
 	}
 
 	for(ip4p=ip4s;ip4p<=ip4e;ip4p++){
 		sprintf(ethip,"%s.%d",ip123,ip4p);
-		printf("%s\n",ethip);
+		//printf("%s\n",ethip);
 		fds=socket(AF_INET,SOCK_STREAM,0);
 		bzero((char *)&client_addr, sizeof(client_addr));
 		client_addr.sin_family = AF_INET;
@@ -76,9 +76,9 @@ int main(int argc,char*argv[]){
 		client_addr.sin_port = htons(502);
 		ret=connect(fds,(struct sockaddr *)&client_addr,sizeof(client_addr));
 		if (ret < 0) {
-			printf("connect ng %s %d\n",ethip,ret);
+			//printf("connect ng %s %d\n",ethip,ret);
 		}else{
-			printf("connect ok %s %d\n",ethip,ret);
+			//printf("connect ok %s %d\n",ethip,ret);
 			sprintf(ethipwk,"%s%n");
 			fputs(ethipwk,fp);
 		}
@@ -87,7 +87,7 @@ int main(int argc,char*argv[]){
 	fclose(fp);
 
 	if(rename(listfilewk, listfile)==0){
-		//remove(listfilewk);
+	
 	}else{
 		fprintf(stderr,"file rename ng %s %s\n",listfilewk,listfile);
 	}
