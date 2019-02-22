6c22d26a23aaa72ffd201c6ea2c61bbdca2652c6
Kumiko Ohashi <kumiko.ohashi@altenergy.co.jp>
2019-02-22 10:15:59 +0900

    ip

--- makelist.c
+++ makelist.c
@@ -22,6 +22,8 @@ int main(int argc,char*argv[]){
 	char *listfile,*ip123;
 	int ip4s,ip4e;
 
+	
+
 	while((opt=getopt(argc,argv,"l:i:s:e:"))!= -1){
 		if(argc != 4){
 			printf("param %d\n",argc);
@@ -43,48 +45,49 @@ int main(int argc,char*argv[]){
 			break;
 		}
 
-		FILE *fp;
-		int ip4p,fds,ret;
-		char ethip[30],listfilewk[256];
-		ethip[0]='\0';
-		listfilewk[0]='\0';
-		struct ifreq ifr;
-		struct sockaddr_in client_addr;
-
-		sprintf(listfilewk,"%s_wk",listfile);
-
-		fp = fopen(listfilewk,"w");
-		if(fp==NULL){
-			printf("file open(create) %s\n",listfilewk);
-			fprintf(stderr,"file open(create) %s\n",listfilewk);
-			return -1;
-		}
-
-		for(ip4p=ip4s;ip4p<=ip4e;ip4p++){
-			sprintf(ethip,"%s.%d",ip123,ip4p);
-			fds=socket(AF_INET,SOCK_STREAM,0);
-			bzero((char *)&client_addr, sizeof(client_addr));
-			client_addr.sin_family = AF_INET;
-			client_addr.sin_addr.s_addr = inet_addr(ethip);
-			client_addr.sin_port = htons(502);
-			ret=connect(fds,(struct sockaddr *)&client_addr,sizeof(client_addr));
-			if (ret < 0) {
-				printf("connect ng %s %d\n",ethip,ret);
-			}else{
-				printf("connect ok %s %d\n",ethip,ret);
-				fputs(ethip,fp);
-			}
-		}
-
-		fclose(fp);
-
-		if(rename(listfilewk, listfile)==0){
-			remove(listfilewk);
-		}else{
-			fprintf(stderr,"file rename %s %s\n",listfilewk,listfile);
-		}
-
-
 	}
+
+	FILE *fp;
+	int ip4p,fds,ret;
+	char ethip[30],listfilewk[256];
+	ethip[0]='\0';
+	listfilewk[0]='\0';
+	struct ifreq ifr;
+	struct sockaddr_in client_addr;
+
+	sprintf(listfilewk,"%s_wk",listfile);
+
+	fp = fopen(listfilewk,"w");
+	if(fp==NULL){
+		printf("file open(create) %s\n",listfilewk);
+		fprintf(stderr,"file open(create) %s\n",listfilewk);
+		return -1;
+	}
+
+	for(ip4p=ip4s;ip4p<=ip4e;ip4p++){
+		sprintf(ethip,"%s.%d",ip123,ip4p);
+		fds=socket(AF_INET,SOCK_STREAM,0);
+		bzero((char *)&client_addr, sizeof(client_addr));
+		client_addr.sin_family = AF_INET;
+		client_addr.sin_addr.s_addr = inet_addr(ethip);
+		client_addr.sin_port = htons(502);
+		ret=connect(fds,(struct sockaddr *)&client_addr,sizeof(client_addr));
+		if (ret < 0) {
+			printf("connect ng %s %d\n",ethip,ret);
+		}else{
+			printf("connect ok %s %d\n",ethip,ret);
+			fputs(ethip,fp);
+		}
+	}
+
+	fclose(fp);
+
+	if(rename(listfilewk, listfile)==0){
+		remove(listfilewk);
+	}else{
+		fprintf(stderr,"file rename %s %s\n",listfilewk,listfile);
+	}
+
+
 
 }
